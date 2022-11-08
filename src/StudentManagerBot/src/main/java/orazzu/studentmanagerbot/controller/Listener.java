2778a963ad23b8df27b8d432420d561947b35cb6
0razzu <0razzu@protonmail.com>
2022-11-08 20:38:19 +0300

    bot iface sketch

--- src/StudentManagerBot/src/main/java/orazzu/studentmanagerbot/controller/Listener.java
+++ src/StudentManagerBot/src/main/java/orazzu/studentmanagerbot/controller/Listener.java
@@ -3,9 +3,12 @@ package orazzu.studentmanagerbot.controller;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import orazzu.studentmanagerbot.service.StudentService;
 import org.slf4j.Logger;
@@ -31,29 +34,55 @@ public class Listener implements UpdatesListener {
         for (Update update: updates) {
             LOGGER.debug("Got update: {}", update);
             
-            Long userId = update.message().chat().id();
-            BaseRequest<SendMessage, SendResponse> answer = null;
+            List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> actions = null;
             
-            final String text = update.message().text();
-            if (text != null) {
-                if (text.startsWith("/")) {
-                    answer = switch (text) {
-                        case "/start" -> studentService.register(
-                                userId,
-                                update.message().chat().username()
-                        );
-                        
-                        default -> studentService.unknownCmd(userId);
-                    };
-                }
+            final Message msg = update.message();
+            
+            if (msg != null) {
+                if (msg.text().startsWith("/"))
+                    actions = processCmd(msg);
                 
                 else
-                    answer = studentService.notCmd(userId);
-                
-                bot.execute(answer);
+                    actions = processText(msg);
             }
+            
+            else if (update.callbackQuery() != null) {
+                actions = processCallback(update.callbackQuery());
+            }
+            
+            for (BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse> action: actions)
+                bot.execute(action);
         }
         
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+    
+    
+    private List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> processCmd(Message msg) {
+        final String text = msg.text();
+        final Long userId = msg.chat().id();
+        final String username = msg.chat().username();
+        
+        return switch (text) {
+            case "/start" -> studentService.register(userId, username);
+            
+            case "/get_subject" -> studentService.getSubjects(userId, username);
+            
+            case "/get_rating" -> studentService.getRating(userId, username);
+            
+            case "/get_hometasks" -> studentService.getHometasks(userId, username);
+            
+            default -> studentService.unknownCmd(userId);
+        };
+    }
+    
+    
+    private List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> processCallback(CallbackQuery query) {
+        return studentService.unknownCallback(query.from().id(), query.message());
+    }
+    
+    
+    private List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> processText(Message msg) {
+        return studentService.notCmd(msg.chat().id());
     }
 }
--- src/StudentManagerBot/src/main/java/orazzu/studentmanagerbot/service/StudentService.java
+++ src/StudentManagerBot/src/main/java/orazzu/studentmanagerbot/service/StudentService.java
@@ -1,28 +1,75 @@
 package orazzu.studentmanagerbot.service;
 
 
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardRemove;
 import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.EditMessageReplyMarkup;
+import com.pengrad.telegrambot.request.EditMessageText;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 
 public class StudentService {
     private final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
     
     
-    public BaseRequest<SendMessage, SendResponse> register(Long userId, String userName) {
-        return new SendMessage(userId, "Вы зарегистрированы");
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> register(Long userId, String username) {
+        return List.of(new SendMessage(userId, "Вы зарегистрированы"));
     }
     
     
-    public BaseRequest<SendMessage, SendResponse> unknownCmd(Long userId) {
-        return new SendMessage(userId, "Неизвестная команда");
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> getSubjects(Long userId, String username) {
+        return List.of(new SendMessage(userId, "Выберите предмет")
+                .replyMarkup(new InlineKeyboardMarkup(
+                                new InlineKeyboardButton("<Название предмета 1>").callbackData("1"),
+                                new InlineKeyboardButton("<Название предмета 2>").callbackData("2")
+                        )
+                ));
     }
     
     
-    public BaseRequest<SendMessage, SendResponse> notCmd(Long userId) {
-        return new SendMessage(userId, "Я понимаю только команды");
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> getRating(Long userId, String username) {
+        return List.of(new SendMessage(userId, "Выберите предмет")
+                .replyMarkup(new InlineKeyboardMarkup(
+                                new InlineKeyboardButton("<Название предмета 1>").callbackData("1"),
+                                new InlineKeyboardButton("<Название предмета 2>").callbackData("2")
+                        )
+                ));
+    }
+    
+    
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> getHometasks(Long userId, String username) {
+        return List.of(new SendMessage(userId, "Выберите предмет")
+                .replyMarkup(new InlineKeyboardMarkup(
+                                new InlineKeyboardButton("<Название предмета 1>").callbackData("1"),
+                                new InlineKeyboardButton("<Название предмета 2>").callbackData("2")
+                        )
+                ));
+    }
+    
+    
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> unknownCmd(Long userId) {
+        return List.of(new SendMessage(userId, "Неизвестная команда"));
+    }
+    
+    
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> unknownCallback(Long userId, Message callbackMsg) {
+        return List.of(
+                new SendMessage(userId, "Ой, во что это вы тыкнули?.."),
+                new EditMessageText(userId, callbackMsg.messageId(), callbackMsg.text()).replyMarkup(new InlineKeyboardMarkup())
+        );
+    }
+    
+    
+    public List<BaseRequest<? extends BaseRequest<?, ?>, ? extends BaseResponse>> notCmd(Long userId) {
+        return List.of(new SendMessage(userId, "Я понимаю только команды"));
     }
 }
