4c01d40e9784b443c5a89c286976f3a6a5da297e
EnergyNick <nsikachin@gmail.com>
2022-11-09 20:05:12 +0300

    Added cache to statements by subject id

--- src/StudentManager/StudentManager.Logic/Wrappers/Implementations/StatementsTableWrapper.cs
+++ src/StudentManager/StudentManager.Logic/Wrappers/Implementations/StatementsTableWrapper.cs
@@ -1,4 +1,5 @@
-﻿using LazyCache;
+﻿using FluentResults;
+using LazyCache;
 using Serilog;
 using StudentManager.Tables;
 using StudentManager.Tables.Models;
@@ -7,8 +8,34 @@ namespace StudentManager.Logic.Wrappers.Implementations;
 
 public class StatementsTableWrapper : BaseTableWrapper<StatementSheetData>
 {
+    private readonly string _cacheDictBySubjectIdKey;
     public StatementsTableWrapper(IManagerSheetEditor<StatementSheetData> sheet, IAppCache appCache, ILogger logger)
         : base(sheet, appCache, logger)
     {
+        _cacheDictBySubjectIdKey = $"{GetType().Name}-BySubjectId";
     }
+
+    protected override async Task<List<StatementSheetData>> UpdateCache()
+    {
+        var result = await base.UpdateCache();
+        AppCache.Add(_cacheDictBySubjectIdKey,
+            result.GroupBy(x => x.IdSubject).ToDictionary(x => x.Key, x => x.ToArray()), GetCacheOptions());
+        return result;
+    }
+
+    public virtual async Task<Result<StatementSheetData[]>> ReadBySubjectId(string subjectId)
+    {
+        if (!AppCache.TryGetValue<Dictionary<string, StatementSheetData[]>>(_cacheDictBySubjectIdKey, out var dict))
+        {
+            await UpdateCache();
+            dict = AppCache.Get<Dictionary<string, StatementSheetData[]>>(_cacheDictBySubjectIdKey);
+        }
+
+        return dict.TryGetValue(subjectId, out var value)
+            ? Result.Ok(value)
+            : Result.Fail(CantFindSubjectErrorMessage(subjectId));
+    }
+
+    private string CantFindSubjectErrorMessage(string id) =>
+        $"Can't find statement in {GetType().Name} by subject id {id}";
 }
