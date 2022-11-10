f654358f4425735b9c8890e9e47bb2bd5a8682e3
EnergyNick <nsikachin@gmail.com>
2022-11-10 14:47:32 +0300

    Added additional indexes for teachers and subjects

--- src/StudentManager/StudentManager.Logic/Wrappers/Implementations/SubjectsTableWrapper.cs
+++ src/StudentManager/StudentManager.Logic/Wrappers/Implementations/SubjectsTableWrapper.cs
@@ -9,17 +9,22 @@ namespace StudentManager.Logic.Wrappers.Implementations;
 public class SubjectsTableWrapper : BaseTableWrapper<AcademicSubjectData>
 {
     private readonly string _cacheDictByGroupIdKey;
+    private readonly string _cacheDictByTeacherIdKey;
 
     public SubjectsTableWrapper(IManagerSheetEditor<AcademicSubjectData> sheet, IAppCache appCache, ILogger logger)
         : base(sheet, appCache, logger)
     {
         _cacheDictByGroupIdKey = $"{GetType().Name}-ByGroupId";
+        _cacheDictByTeacherIdKey = $"{GetType().Name}-ByTeacherId";
     }
 
     protected override async Task<List<AcademicSubjectData>> UpdateCache()
     {
         var result = await base.UpdateCache();
         AppCache.Add(_cacheDictByGroupIdKey, result.GroupBy(x => x.IdGroup).ToDictionary(x => x.Key, x => x.ToArray()),
+            GetCacheOptions());
+        AppCache.Add(_cacheDictByTeacherIdKey,
+            result.GroupBy(x => x.IdTeacher).ToDictionary(x => x.Key, x => x.ToArray()),
             GetCacheOptions());
         return result;
     }
@@ -36,6 +41,22 @@ public class SubjectsTableWrapper : BaseTableWrapper<AcademicSubjectData>
             return Result.Fail<AcademicSubjectData[]>(WrapperErrors.EmptyInGoogleTablesCache);
 
         return dict.TryGetValue(groupId, out var value)
+            ? value
+            : Array.Empty<AcademicSubjectData>();
+    }
+
+    public virtual async Task<Result<AcademicSubjectData[]>> ReadByTeacherId(string teacherId)
+    {
+        if (!AppCache.TryGetValue<Dictionary<string, AcademicSubjectData[]>>(_cacheDictByTeacherIdKey, out var dict))
+        {
+            await UpdateCache();
+            dict = AppCache.Get<Dictionary<string, AcademicSubjectData[]>>(_cacheDictByTeacherIdKey);
+        }
+
+        if (dict == null)
+            return Result.Fail<AcademicSubjectData[]>(WrapperErrors.EmptyInGoogleTablesCache);
+
+        return dict.TryGetValue(teacherId, out var value)
             ? value
             : Array.Empty<AcademicSubjectData>();
     }
